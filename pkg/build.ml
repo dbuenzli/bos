6644e954de708537fc316573688d6f0b3f00dc84
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-03-02 23:03:36 +0100

    Add toplevel support.

--- pkg/build.ml
+++ pkg/build.ml
@@ -8,6 +8,7 @@ let () =
   Pkg.describe "bos" ~builder:`OCamlbuild [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/bos";
+    Pkg.lib ~exts:Exts.library "src/bos_top";
     Pkg.lib ~cond:unix ~exts:Exts.module_library "src/bos_unix";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md"; ]
