93e79c03302f42990145058fac5d5c0cd8450dff
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-01-08 14:51:31 +0100

    Remove OS.Time (closes #18).

--- src/bos.ml
+++ src/bos.ml
@@ -27,50 +27,6 @@ module OS = struct
   module Dir = Bos_os_dir
   module Cmd = Bos_os_cmd
   module U = Bos_os_u
-
-  (* FIXME remove this. *)
-
-  module Time = struct
-    type posix_s = float
-    type tz_offset_s = float
-
-    let now_s = Unix.gettimeofday
-
-    let tz_offset_s t =
-      let utc = Unix.gmtime t in
-      let local = Unix.localtime t in
-      let dd = local.Unix.tm_yday - utc.Unix.tm_yday in
-      let dh = local.Unix.tm_hour - utc.Unix.tm_hour in
-      let dm = dh * 60 + (local.Unix.tm_min - utc.Unix.tm_min) in
-      let dm =
-        if dd = 1 || dd < -1 (* year wrap *) then dm + (24 * 60) else
-        if dd = -1 || dd > 1 (* year wrap *) then dm - (24 * 60) else
-        dm  (* same day *)
-      in
-      float (60 * dm)
-
-    let current_tz_offset_s () = tz_offset_s (now_s ())
-
-    let pp_stamp ?(human = false) ?(tz_offset_s = 0.) ppf t =
-      (* RFC 3339 is written in local time w.r.t. to the offset, so we
-         add the offset to the stamp to render the calendar fields *)
-      let local_t = t +. tz_offset_s in
-      let c = Unix.gmtime local_t in
-      let int_of_round x = truncate (floor (x +. 0.5)) in
-      let tz_min = int_of_round (tz_offset_s /. 60.) in
-      let tsep = if human then ' ' else 'T' in
-      let osep = if human then " " else "" in
-      Fmt.pf ppf "%04d-%02d-%02d%c%02d:%02d:%02d%s%c%02d%02d"
-        (c.Unix.tm_year + 1900) (c.Unix.tm_mon + 1) c.Unix.tm_mday
-        tsep
-        c.Unix.tm_hour c.Unix.tm_min c.Unix.tm_sec
-        osep
-        (if tz_min < 0 then '-' else '+') (tz_min / 60) (tz_min mod 60)
-
-    let pp_stamp_now ?human ppf () =
-      let now = now_s () in
-      pp_stamp ?human ~tz_offset_s:(tz_offset_s now) ppf now
-  end
 end
 
 (*---------------------------------------------------------------------------
