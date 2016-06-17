70bd7f10e6af3cde10cd9e9de429b35429d67a10
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-06-17 19:52:55 +0100

    topkg: use test support.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -8,4 +8,8 @@ let () =
   Ok [ Pkg.mllib ~api:["Bos"] "src/bos.mllib";
        Pkg.mllib "src/bos_setup.mllib";
        Pkg.mllib ~api:[] "src/bos_top.mllib";
-       Pkg.lib "src/bos_top_init.ml"; ]
+       Pkg.lib "src/bos_top_init.ml";
+       Pkg.test "test/test";
+       Pkg.test ~run:false "test/test_arg";
+       Pkg.test ~run:false "test/test_arg_pos";
+       Pkg.test ~run:false "test/watch"; ]
