5de52603eaa5e64a833539b266a67968a4c4a7ae
Shakthi Kannan <author@shakthimaan.com>
2021-08-05 18:22:17 +0530

    Use toploop.load_file for 4.14.0+trunk

--- src/bos_top.ml
+++ src/bos_top.ml
@@ -4,7 +4,7 @@
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
-let () = ignore (Toploop.use_file Format.err_formatter "bos_top_init.ml")
+let () = ignore (Toploop.load_file Format.err_formatter "bos_top_init.ml")
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2015 Daniel C. Bünzli
